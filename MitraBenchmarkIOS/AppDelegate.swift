7778e673666f1803364b6619886f931a08ec3c90
Serhiy Butz <serhiybutz@gmail.com>
2022-11-02 01:28:29 -0400

    Bring everything in line with renaming the account.

--- MitraBenchmarkIOS/AppDelegate.swift
+++ MitraBenchmarkIOS/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  MitraBenchmarkIOS
 //
-//  Created by Serge Bouts on 7/1/20.
+//  Created by Serhiy Butz on 7/1/20.
 //  Copyright © 2020 iRiZen.com. All rights reserved.
 //
 
